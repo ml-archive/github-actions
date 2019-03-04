bbdb433c83983bfccef47a05c1770235211c91b3
Rasmus Ebbesen <re@nodes.dk>
2019-03-04 10:28:07 +0100

    Checkout gh pages, commit and push

--- actions/jazzy-docs/entrypoint.sh
+++ actions/jazzy-docs/entrypoint.sh
@@ -3,6 +3,10 @@
 git config --global user.name = "$GH_USER"
 git config --global user.email = $GH_EMAIL
 
-
+git checkout -B gh-pages
+git status
+git add . --force
+git commit -m "Jazzy docs updated"
+git push origin gh-pages
 
 ls -lisa
